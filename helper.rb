f39f4c34e078aabc52be6fd7999360ca1f5d879a
Ruben <ruben.jenster@inovex.de>
2011-10-03 20:59:34 +0200

    simple sinatra app for testing the library/adding show-cases

---
+++ helper.rb
@@ -0,0 +1,15 @@
+$:<<(File.join(File.dirname(__FILE__), 'lib'))
+
+require 'rubygems' # required for ruby 1.8 and jruby
+gem 'tilt','=1.2.2' # sinatra erb support doesn't work with tilt 1.3.x
+require 'mongo'
+require 'sinatra'
+
+get '/time' do
+  Time.now.to_s
+end
+
+get '/:page' do
+  erb params[:page].to_sym
+end
+
